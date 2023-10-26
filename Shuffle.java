cd758fbfa7d7bd2165f7d3a1d669cb8d48f12a4b
pruthvi12-a <71586944+pruthvi12-a@users.noreply.github.com>
2023-10-26 20:53:28 +0530

    Add files via upload

---
+++ Shuffle.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Shuffle {
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
+        List<Integer> list = Arrays.asList(arr);
+        Collections.shuffle(list);
+
+        System.out.println("Shuffled : " + Arrays.toString(list.toArray()));
+    }
+}
